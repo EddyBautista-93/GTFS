afade39782a88927d8a5fd76e11edbb0bc2f3b8b
Eddy Bautista <bautista.j.eddy@gmail.com>
2020-02-02 11:34:11 -0600

    first commit

---
+++ src/App.js
@@ -0,0 +1,47 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+
+
+var GtfsRealtimeBindings = require('gtfs-realtime-bindings');
+var request = require('request');
+
+var requestSettings = {
+  method: 'GET',
+  url: http://gtfs.viainfo.net/gtfs-realtime/trapezerealtimefeed.pb,
+  encoding: null
+};
+request(requestSettings, function (error, response, body) {
+  if (!error && response.statusCode == 200) {
+    var feed = GtfsRealtimeBindings.transit_realtime.FeedMessage.decode(body);
+    feed.entity.forEach(function(entity) {
+      if (entity.trip_update) {
+        console.log(entity.trip_update);
+      }
+    });
+  }
+});
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
